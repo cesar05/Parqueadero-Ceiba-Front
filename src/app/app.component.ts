eaeda18fe6a58804152dc5e0ea810a3cfb6b84f8
César Augusto Muñoz Roldán - Ceiba Software <cesar.munoz@ceiba.com.co>
2018-06-26 11:39:36 -0500

    FrontEnd del parqueadero-ceiba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, OnInit, Input } from '@angular/core';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
+import { VehiculoIngresarComponent } from './vehiculo-ingresar/vehiculo-ingresar.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Ceiba Software House';
+
+  ngOnInit() {
+  }
 }
